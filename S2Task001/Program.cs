e5151bf364bc712f14c21cb2b36b7bfe6c96693a
Larin <lagoss@yandex.ru>
2023-03-10 23:15:47 +0300

    add tasks from seminar 2 and .gitignore

---
+++ S2Task001/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число
+// и удаляет вторую цифру этого числа
+
+// Console.Write("Введите число: ");
+// // int num = int.Parse(Console.Readline()!); //435 -> 46 (4 * 10 + %10)
+// int num = new Random().Next(100, 1000);
+// int result = (num / 100) * 10 + num % 10;
+// int num1 = num / 100;
+// int num3 = num % 10;
+// Console.WriteLine($"{num}-> {result}");
+// Console.Write($"{num} -> {num1}{num3}");
+
+// Console.Write($" ")
+// // 1 и 3    
+// // 1 * 10 + 3
+
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine()!;
+if(number.Length == 3){
+    Console.WriteLine($"{number} -> {number[0]}{number[2]}!");
+}
+else
+Console.WriteLine("Введено неверное чило ");
+
---
+++ S2Task002/Program.cs
@@ -0,0 +1,74 @@
+﻿// 1. Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+// 2. Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. 
+// // Если второе число не кратно числу первому, то программа выводит остаток от деления.
+
+// // 34, 5 -> не кратно, остаток 4
+// // 16, 4  -> кратно
+
+
+// int num = new Random().Next(10, 100);
+// Console.WriteLine(num);
+// int result1 = num/10;
+
+// int result2 = num%10;
+
+// if (result1 > result2){
+//     Console.WriteLine($" Первый знак введенного числа {result1} больше чем второй знак {result2} ");
+// }
+// else{
+//     Console.WriteLine($" Второй знак введенного числа больше первого или равен ему ");
+// }
+
+// Console.WriteLine("Введите два числа: ");
+// int number1 = int.Parse(Console.ReadLine()!);
+// int number2 = int.Parse(Console.ReadLine()!);
+
+// if(number2%number1 == 0){
+//     ;
+//     Console.WriteLine("Числа кратны друг другу");
+// }
+// else{
+//     Console.WriteLine("Числа не кратны друг другу");
+//     Console.WriteLine(number2%number1);
+// }
+
+
+// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+// 14  ->  нет
+// 46  ->  нет
+// 161 ->  да
+
+// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+// Console.WriteLine("Введите число: ");
+// int number = int.Parse(Console.ReadLine()!);
+
+// if (number%7 == 0 && number%23 ==0){
+//     Console.WriteLine("Число кратно и 7 и 23");
+// }
+// else{
+//     Console.WriteLine("Введенное число не кратно 7 и 23");
+// }
+
+Console.WriteLine("Введите два числа ");
+int number1 = int.Parse(Console.ReadLine()!);
+int number2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Квадрат первого числа равен {Math.Pow(number1,2)}, квадрат второго числа равен {Math.Pow(number2,2)}");
+if (number1 == Math.Pow(number2,2) || number2 == Math.Pow(number1,2))
+{
+    if (Math.Pow(number1,2) == number2){
+        Console.WriteLine("Квадрат первого числа равен второму числу");
+    }
+    if (Math.Pow(number2,2) == number1){
+        Console.WriteLine("Квадрат второго числа равен первому числу");
+    }
+}
+else
+{
+    Console.WriteLine("Ни одно из введенных чисел не является квадратом второго");
+}
